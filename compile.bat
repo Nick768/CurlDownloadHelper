b8a8d0dccb605f3b93a76921e95fc4954f2db908
Nick768 <52794307+Nick768@users.noreply.github.com>
2021-06-11 13:40:55 +0200

    Initial version

---
+++ compile.bat
@@ -0,0 +1,8 @@
+@echo off
+
+"C:\Program Files\AutoHotkey\ahkdll\v2\Compiler\ahk2Exe.exe" "C:\Program Files\AutoHotkey\ahkdll\v2\Compiler\ahk2Exe.ahk" /in "DownloadHelper.ahkh2" /bin "C:\Program Files\AutoHotkey\ahkdll\v2\x64w_MT\AutoHotkey.bin" || goto Error
+bin\AddResource.exe bin\curl.exe DownloadHelper.exe || goto Error
+Exit
+
+:Error
+pause
